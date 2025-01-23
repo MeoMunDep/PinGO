33d6094775ab27ad9e0d22a6b7d69f7a72473fe2
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-23 13:18:56 +0700

    this game is ended

--- PinGO/run.sh
+++ PinGO/run.sh
@@ -30,22 +30,17 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "delayEachAccount": [1, 81],
+  "referralCode": "1XA96FUU"
 }
 EOL
 }
 
 check_configs() {
-    if ! node -e "const cfg=require('./configs.json');if(typeof cfg.howManyAccountsRunInOneTime !== 'number' || cfg.howManyAccountsRunInOneTime < 1) throw new Error('Invalid config');" 2>/dev/null; then
+    if ! node -e "const cfg=require('./configs.json');if(typeof cfg.limit !== 'number' || cfg.limit < 1) throw new Error('Invalid config');" 2>/dev/null; then
         print_red "Invalid configuration detected. Resetting to default values..."
         create_default_configs
         print_green "Configuration reset completed."
@@ -54,9 +49,9 @@ check_configs() {
 
 while true; do
     clear
-    echo "================================="
-    echo "    name BOT SETUP AND RUN SCRIPT"
-    echo "================================="
+    echo "==================================================================""
+    echo "    PinGO BOT SETUP AND RUN SCRIPT"
+    echo "==================================================================""
     echo
     echo "Current directory: $(pwd)"
     echo "Node modules directory: $MODULES_DIR/node_modules"
@@ -115,7 +110,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
